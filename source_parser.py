3435ccbd2747157c453297c475181f9c1c64fbdf
Roman <34835155+CuberHuber@users.noreply.github.com>
2023-07-21 15:45:38 +0500

    Initial commit

---
+++ source_parser.py
@@ -0,0 +1,103 @@
+"""
+Парсер плагина SPP
+
+1/2 документ плагина
+"""
+
+import os
+import time
+
+from spp_document import SPP_document
+
+
+class SOURCE_PARSER_CLASS:
+    """
+    Класс парсера плагина SPP
+
+    :warning Все необходимое для работы парсера должно находится внутри этого класса
+
+    :_content_document: Это список объектов документа. При старте класса этот список должен обнулиться,
+                        а затем по мере обработки источника - заполняться.
+
+
+    """
+
+    _content_document: list[SPP_document]
+
+    def __init__(self, *args, **kwargs):
+        """
+        Конструктор класса парсера
+
+        По умолчанию внего ничего не передается, но если требуется (например: driver селениума), то нужно будет
+        заполнить конфигурацию
+        """
+        # Обнуление списка
+        self._content_document = []
+        ...
+
+    def content(self) -> list[SPP_document]:
+        """
+        Главный метод парсера. Его будет вызывать платформа. Он вызывает метод _parse и возвращает список документов
+        :return:
+        :rtype:
+        """
+        self._parse()
+        return self._content_document
+
+    def _parse(self):
+        """
+        Метод, занимающийся парсингом. Он добавляет в _content_document документы, которые получилось обработать
+        :return:
+        :rtype:
+        """
+        ...
+
+    @staticmethod
+    def some_necessary_method():
+        """
+        Если для парсинга нужен какой-то метод, то его нужно писать в классе.
+
+        Например: конвертация дат и времени, конвертация версий документов и т. д.
+        :return:
+        :rtype:
+        """
+        ...
+
+    @staticmethod
+    def nasty_download(driver, path: str, url: str) -> str:
+        """
+        Метод для "противных" источников. Для разных источника он может отличатся.
+        Но основной его задачей является:
+            доведение driver селениума до файла непосредственно.
+
+            Например: пройти куки, ввод форм и т. п.
+
+        Метод скачивает документ по пути, указанному в driver, и возвращает имя файла, который был сохранен
+        :param driver: WebInstallDriver, должен быть с настроенным местом скачивания
+        :_type driver: WebInstallDriver
+        :param url:
+        :_type url:
+        :return:
+        :rtype:
+        """
+
+        with driver:
+            driver.set_page_load_timeout(40)
+            driver.get(url=url)
+            time.sleep(1)
+
+            # ========================================
+            # Тут должен находится блок кода, отвечающий за конкретный источник
+            # -
+            # ---
+            # ========================================
+
+            # Ожидание полной загрузки файла
+            while not os.path.exists(path + '/' + url.split('/')[-1]):
+                time.sleep(1)
+
+            if os.path.isfile(path + '/' + url.split('/')[-1]):
+                # filename
+                return url.split('/')[-1]
+            else:
+                return ""
---
+++ spp_document.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from datetime import datetime
+from hashlib import sha256
+
+
+@dataclass
+class SPP_document:
+    """
+    Объект документа в SPP
+    """
+    doc_id: int | None
+    title: str
+    abstract: str | None
+    text: str | None
+    web_link: str
+    local_link: str | None
+    other_data: dict | None
+    pub_date: datetime
+    load_date: datetime
+
+    @property
+    def hash(self):
+        """
+        Для проверки уникальности и новизны документа.
+
+        :return:
+        :rtype:
+        """
+        concat_name = self.title + '_' + self.web_link + '_' + str(self.pub_date.timestamp())
+        return sha256(concat_name.encode('utf8')).digest()
